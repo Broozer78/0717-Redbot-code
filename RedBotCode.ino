3673ee669eba6545471e0dbc1a3cd42e7aeded4d
Broozer78 <brucehswe@gmail.com>
2025-02-17 16:41:28 -0500

    RedBotCode 1.2

--- RedBotCode.ino
+++ RedBotCode.ino
@@ -4,15 +4,25 @@ RedBotMotors motors;
 
 void setup()
 {
+  Serial.begin(9600);
   //third motor
   //pin setup
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
+  pinMode(3, INPUT);
 
+
+  
+  
+}
+
+void loop()
+{
+  /*
   //up
   digitalWrite(11, LOW);
   digitalWrite(10, HIGH);
-  delay(500);
+  delay(450);
 
   digitalWrite(11, HIGH);
   digitalWrite(10, HIGH);
@@ -24,41 +34,64 @@ void setup()
   
   digitalWrite(11,LOW);
   digitalWrite(10,LOW);
+*/
 
-  //main program
-  motors.drive(150);
-  delay(2500);
-  motors.brake();
-  motors.pivot(150, 375);
-  motors.drive(150);
-  delay(1700);
-  motors.brake();
-  motors.pivot(150, 375);
-  motors.drive(150);
-  delay(2200);
-  motors.brake();
-  motors.pivot(-150, 375);
-  motors.drive(150);
-  delay(1000);
-  motors.brake();
-  motors.pivot(-150, 375);
-  motors.drive(150);
-  delay(750);
-  motors.brake();
-  delay(30);
-  motors.drive(225);
-  delay(7500);
-  motors.brake();
-  motors.pivot(150, 375);
-  motors.drive(150);
-  delay(750);
-  motors.brake();
-  
-}
-
-void loop()
-{
-  
+//button press to start
+  if(digitalRead(3)==LOW)
+  {
+    
+    //main program
+    motors.drive(150);
+    delay(2000);
+    digitalWrite(11, LOW);
+    digitalWrite(10, HIGH);
+    delay(400);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    motors.brake();
+    motors.pivot(150, 375);
+    motors.drive(150);
+    delay(1700);
+    motors.brake();
+    digitalWrite(11, HIGH);
+    digitalWrite(10, LOW);
+    delay(300);
+    digitalWrite(11,LOW);
+    digitalWrite(10,LOW);
+    motors.pivot(150, 375);
+    motors.drive(150);
+    delay(2300);
+    motors.brake();
+    motors.pivot(-150, 375);
+    motors.drive(150);
+    delay(1000);
+    motors.brake();
+    motors.pivot(-150, 375);
+    motors.drive(150);
+    delay(750);
+    motors.brake();
+    digitalWrite(11, LOW);
+    digitalWrite(10, HIGH);
+    delay(400);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    delay(30);
+    motors.drive(225);
+    delay(7500);
+    motors.brake();
+    digitalWrite(11, HIGH);
+    digitalWrite(10, LOW);
+    delay(300);
+    digitalWrite(11,LOW);
+    digitalWrite(10,LOW);
+    motors.pivot(150, 375);
+    motors.drive(150);
+    delay(750);
+    motors.brake();
+    
+    Serial.println("Button is Pressed");
+    delay(100);
+  }
 }
 
 
