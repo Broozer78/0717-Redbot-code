8dcf50000c5d46a047edfacd538033af1d1c9b60
Broozer78 <brucehswe@gmail.com>
2025-02-18 15:31:53 -0500

    RedBotCode 1.4

--- RedBotCode.ino
+++ RedBotCode.ino
@@ -1,45 +1,65 @@
+#include <RedBot.h>
 
-#include <RedBot.h> 
-
-RedBotMotors motors;
+RedBotMotors motors; 
 
 void setup()
 {
-
+  Serial.begin(9600);
+  //third motor
   //pin setup
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
-
-  //motors.pivot(150, 375);
-  motors.drive(150);
-  delay(2850);
-  motors.brake();
-  motors.pivot(150, 375);
-  motors.drive(150);
-  delay(2100);
-  motors.brake();
-  motors.pivot(150, 475);
-  motors.drive(150);
-  delay(2350);
-  motors.brake();
-  motors.pivot(-150, 450);
-  motors.drive(150);
-  delay(1000);
-  motors.brake();
-  motors.pivot(-150, 375);
- // motors.drive(150);
-  //delay(1000);
-  //motors.brake();
-  //motors.pivot(-150, 400);
+  pinMode(3, INPUT);
 
 
-  motors.drive(255);
-  delay(6000);
-  motors.brake();
-  motors.pivot(150, 375);
-  motors.drive(150);
-  delay(500);
-  motors.brake();
+  
+  
+}
+
+void loop()
+{
+
+//button press to start
+  if(digitalRead(3)==LOW)
+  {
+    //main program
+    //motors.pivot(150, 375);
+    motors.drive(150);
+    up();
+    delay(2850);
+    motors.brake();
+    motors.pivot(150, 375);
+    motors.drive(150);
+    down();
+    delay(2100);
+    motors.brake();
+    motors.pivot(150, 475);
+    motors.drive(150);
+    delay(2350);
+    motors.brake();
+    motors.pivot(-150, 450);
+    motors.drive(150);
+    delay(1000);
+    motors.brake();
+    motors.pivot(-150, 375);
+    //motors.drive(150);
+    //delay(1000);
+    //motors.brake();
+    //motors.pivot(-150, 400);
+    up();
+
+    motors.drive(255);
+    delay(6000);
+    motors.brake();
+    motors.pivot(150, 375);
+    motors.drive(150);
+    delay(500);
+    motors.brake();
+    down();
+    
+    Serial.println("Button is Pressed");
+    delay(100);
+  }
 }
 
 void up()
@@ -61,9 +81,6 @@ void down()
   digitalWrite(11,LOW);
   digitalWrite(10,LOW);
 }
-
-void loop(){}
-
 
 
 
